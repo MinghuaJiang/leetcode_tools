659d2424d09ecf37b5b6c1f32520108d710669f8
Minghua Jiang <61221630@qq.com>
2017-03-22 13:52:36 -0400

    add java support

---
+++ src/main/java/rest_client/RestClient.java
@@ -0,0 +1,12 @@
+package rest_client;
+
+/**
+ * Created by cutehuazai on 3/22/17.
+ */
+public class RestClient {
+    private String serviceUrl;
+
+    public static void main(String[] args) {
+
+    }
+}
